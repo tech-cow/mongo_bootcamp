9ddb18b5e5289b63183c6f2570bf541e60a60372
Yu Zhou <junior147147@yahoo.com>
2018-03-27 15:25:41 -0700

    readme

---
+++ bootcamp/users/test/test_helper.js
@@ -0,0 +1,10 @@
+// This is basically include, save the module to a const, then we can access its API.
+const mongoose = require('mongoose');
+
+//Tell Mongoose to connect to our MongoDB
+mongoose.connect('mongodb://localhost/users_test');
+mongoose.connection
+  .once('open', () => console.log('Good to go!'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
